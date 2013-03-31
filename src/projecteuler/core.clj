48d34a3b27f56eca6056ed7cad530258bff2d397
Alex <nabrozidhs@gmail.com>
2013-03-31 16:34:00 +0100

    Solved problem 2.

--- src/projecteuler/core.clj
+++ src/projecteuler/core.clj
@@ -5,5 +5,18 @@
   "Find the sum of all the multiples of 3 or 5 below x."
   [x]
   (reduce + 
-  	(filter #(or (zero? (rem % 3))
-  		         (zero? (rem % 5))) (range x))))
+    (filter #(or (zero? (rem % 3))
+                 (zero? (rem % 5))) (range x))))
+
+(defn problem2
+  "Find the sum of the even-valued fibonacci terms below x"
+  [x]
+  (reduce +
+    (filter even?
+      (loop [a 1
+             b 2
+             res [a b]]
+        (let [next (+ a b)]
+          (if (>= next x)
+            res
+            (recur b next (conj res next))))))))
--- test/projecteuler/core_test.clj
+++ test/projecteuler/core_test.clj
@@ -5,4 +5,8 @@
 (deftest a-test
   (testing "Problem 1."
     (is (= 23 (problem1 10)))
-    (is (= 233168 (problem1 1000)))))
+    (is (= 233168 (problem1 1000))))
+
+  (testing "Problem 2."
+    (is (= 10 (problem2 10)))
+    (is (= 4613732 (problem2 4000000)))))
